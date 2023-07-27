90efd485e4b60ee1689619b873860e7c5e0f8cc5
ZlatinIvanov <zlatin89@gmail.com>
2023-07-27 09:26:52 +0300

    Create 8. Prime Numbers.py

---
+++ 08. Iterators-and-Generators Exercise/8. Prime Numbers.py
@@ -0,0 +1,11 @@
+def get_primes(numbers: list):
+    for number in numbers:
+        if number <=1:
+            continue
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                break
+        else:
+            yield number
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
