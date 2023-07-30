d424b57fbbf3764a93c7bac75c8f4861c101c354
ZlatinIvanov <zlatin89@gmail.com>
2023-07-30 10:52:46 +0300

    Update 1. Photo Album.py

--- 05. Static-and-Class-Methods Exercise/1. Photo Album.py
+++ 05. Static-and-Class-Methods Exercise/1. Photo Album.py
@@ -1,18 +1,29 @@
+import math
 from typing import List
 
 
 class PhotoAlbum:
     def __init__(self, pages: int):
         self.pages = pages
-        self.photos: List[str] = []
+        self.photos: List[List[str]] = self.__initialize_photos()
+        self.current_row_index = 0
+
+    def __initialize_photos(self) -> List[List[str]]:
+        matrix = []
+        for _ in range(self.pages):
+            matrix.append([])
+        return matrix
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        pass
+        pages = math.ceil(photos_count / 4)
+        return cls(pages)
 
     def add_photo(self, label: str):
-        pass
+        self.photos[self.current_row_index].append(label)
 
     def display(self):
         pass
 
+p = PhotoAlbum(2)
+a = 5
