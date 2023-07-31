177f42d6adfdf0141752c2e472649014835307be
ZlatinIvanov <zlatin89@gmail.com>
2023-07-31 13:37:36 +0300

    Update dvd.py

--- 05. Static-and-Class-Methods Exercise/2. Movie World/project/dvd.py
+++ 05. Static-and-Class-Methods Exercise/2. Movie World/project/dvd.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class DVD:
 
     def __init__(self, name: str, dvd_id: int, creation_year: int, creation_month: int, age_restriction: int):
@@ -8,15 +11,19 @@ class DVD:
         self.age_restriction = age_restriction
         self.is_rented = False
 
-    @staticmethod
-    def from_date(self, dvd_id: int, name: str, date: str, age_restriction: int):
-        self.id = dvd_id
-        self.name = name
-        self.date = date
-        self.age_restriction = age_restriction
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        # mapper = {1: "January", 2: "February", 3: "March", 4: "April"}
+        day, month_number_as_string, year = date.split(".")
+        # month = mapper[int(month_number)]
+        monthinteger = int(month_number_as_string)
+        month = datetime.date(1900, monthinteger, 1).strftime('%B')
+
+        return cls(name, id, int(year), month, age_restriction)
 
     def __repr__(self):
+        rented_status = 'rented' if self.is_rented else 'not rented'
         return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year})" \
-               f" has age restriction {self.age_restriction}. Status: {self.is_rented}"
+               f" has age restriction {self.age_restriction}. Status: {rented_status}"
         
         
