8b09cb23637623bb2d7589cc8f360d5d67aff192
ZlatinIvanov <zlatin89@gmail.com>
2023-08-13 08:33:41 +0300

    Create animal.py

---
+++ 06. Polymorphism and Abstraction Exercise/5. Animals/project/animal.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+
+    def __init__(self, name: str, age: int, gender: str):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    @abstractmethod
+    def make_sound(self):
+        ...
+
+    @property
+    @abstractmethod
+    def __repr__(self):
+        ...
+
+
