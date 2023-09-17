28f4f1e4c6447d14bdaa9a1d950817442d248911
ZlatinIvanov <zlatin89@gmail.com>
2023-09-17 10:41:06 +0300

    Create student.py

---
+++ 15. Exam/Bank/project/clients/student.py
@@ -0,0 +1,13 @@
+from project.clients.base_client import BaseClient
+
+
+class Student(BaseClient):
+
+    INITIAL_INTEREST = 2
+
+    def __init__(self, name: str, client_id: str, income: float):
+        super().__init__(name, client_id, income, Student.INITIAL_INTEREST)
+        self.interest = Student.INITIAL_INTEREST
+
+    def increase_clients_interest(self):
+        self.interest += 1
